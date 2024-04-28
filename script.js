a6ccb43795531c5c4a4b7207f04f9a03ecb6e94e
Nick Rabone <n.rabone72@aol.com>
2024-04-28 13:44:51 +0100

    Earth rotation animation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const links = [...document.querySelectorAll('.nav-link')]
 const navText = ['Home', 'About Us', 'Projects', 'Documentation','Contact']
+const navImg = document.querySelector('.navbar-img')
 
 links.forEach((link, i) => {
     navText[i].split('').forEach((letter, j) => {
@@ -7,5 +8,8 @@ links.forEach((link, i) => {
         span.textContent = letter
         span.style.cssText = `--i: .${j}s`
         link.append(span)
+    })
+    link.addEventListener('mouseenter', () => {
+        navImg.style.transform = `translate(${link.dataset.position}%, -50%) rotate(${link.dataset.position * 8}deg)`
     })
 })
